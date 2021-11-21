80269edc0c2da2f0781ef5856d60896c12070ae3
Владимир Николаев <94142710+WinHead@users.noreply.github.com>
2021-11-21 23:04:14 +0300

    Create task_22.sql
    Добавить из таблицы supply в таблицу book, все книги, кроме книг, написанных Булгаковым М.А. и Достоевским Ф.М

---
+++ task_22.sql
@@ -0,0 +1,9 @@
+/* Добавить из таблицы supply в таблицу book, все книги, кроме книг, написанных Булгаковым М.А. и Достоевским Ф.М */
+
+INSERT INTO book(title, author, price, amount)
+SELECT title, author, price, amount
+FROM supply
+WHERE author != 'Булгаков М.А.' AND author != 'Достоевский Ф.М.';
+
+
+SELECT * FROM book;
