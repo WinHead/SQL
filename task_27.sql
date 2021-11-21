e21bd0209f8d297512e929dcaae7decb0c2c19d3
Владимир Николаев <94142710+WinHead@users.noreply.github.com>
2021-11-21 23:08:55 +0300

    Create task_27.sql
    Удалить из таблицы supply книги тех авторов, общее количество экземпляров книг которых в таблице book превышает 10

---
+++ task_27.sql
@@ -0,0 +1,4 @@
+/* Удалить из таблицы supply книги тех авторов, общее количество экземпляров книг которых в таблице book превышает 10 */
+
+DELETE FROM supply
+WHERE author IN (SELECT author FROM book WHERE amount >= 10)
