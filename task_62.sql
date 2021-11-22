31eadd7014ec20da551992b2540ff432bf1af217
Владимир Николаев <94142710+WinHead@users.noreply.github.com>
2021-11-22 14:51:21 +0300

    Create task_62.sql
    Удалить все жанры, к которым относится меньше 4-х книг. В таблице book для этих жанров установить значение Null

---
+++ task_62.sql
@@ -0,0 +1,6 @@
+/* Удалить все жанры, к которым относится меньше 4-х книг. В таблице book для этих жанров установить значение Null */
+
+
+
+DELETE FROM genre
+WHERE genre_id IN (SELECT genre_id FROM book GROUP BY genre_id HAVING COUNT(title) < 4)
